0a2bbecaed33321cfd62a3106d3547e4be4622d9
Chamarak <ckchamara22@gmail.com>
2020-03-30 13:04:41 +0530

    initial commit

---
+++ src/com/company/user/artist.java
@@ -0,0 +1,27 @@
+package com.company.user;
+
+public class artist extends user {
+
+    private int noOfSongs;
+    private String gerne;
+
+    public int getNoOfSongs() {
+        return noOfSongs;
+    }
+
+    public void setNoOfSongs(int noOfSongs) {
+        this.noOfSongs = noOfSongs;
+    }
+
+    public String getGerne() {
+        return gerne;
+    }
+
+    public void setGerne(String gerne) {
+        this.gerne = gerne;
+    }
+
+    public void storeAndPrintSongs() {
+
+    }
+}
